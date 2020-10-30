2d8f17bf5e8b3c53dd66e933ee5875e0353bb962
MatKav25 <72069263+MatKav25@users.noreply.github.com>
2020-10-30 09:56:29 +0100

    move

---
+++ Assets/Script/Trigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("object entered the trigger");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("object is within trigger"); 
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("object Exited the trigger");
+    }
+}
